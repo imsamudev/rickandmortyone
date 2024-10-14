f14c891ddd7a599baab7a14b208d025aa1dd0dbe
SamuDev <samudev01@gmail.com>
2024-10-14 13:56:11 -0300

    structure changes Home page

---
+++ src/app/(page)/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import Carousel from "@/components/hero/Hero";
+
+const HomePage = () => {
+    return (
+        <>
+            <Carousel />
+        </>
+    );
+};
+
+export default HomePage;
