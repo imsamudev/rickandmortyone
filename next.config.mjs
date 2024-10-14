a15578ac0953575103d9f095312933b765de717f
SamuDev <samudev01@gmail.com>
2024-10-14 13:52:56 -0300

    next configs, generals optimizations, chunks, strictmode and swc

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,19 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  reactStrictMode: true,
+  swcMinify: true,
+
+  webpack: (config) => {
+    config.optimization.splitChunks = {
+      chunks: "all",
+      minSize: 20000,
+      maxSize: 40000,
+    };
+    config.optimization.runtimeChunk = "single";
+
+    return config;
+  },
 };
 
 export default nextConfig;
