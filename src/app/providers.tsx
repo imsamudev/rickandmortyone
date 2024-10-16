186d7aef8b2f753a45bf4f32e381c811fd761cc3
SamuDev <samudev01@gmail.com>
2024-10-16 19:39:04 -0300

    Update providers, loading screen and errorboundary applied

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,14 +1,63 @@
-"use client";
+'use client';
 
-import { NextUIProvider } from '@nextui-org/react'
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+import ErrorBoundary from '@/components/ErrorBoundary';
+import { NextUIProvider, Spinner } from '@nextui-org/react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import Image from 'next/image';
+import Logo from "@/assets/logo/Logo.svg";
+import { useEffect, useState } from 'react';
+
+const LOADING_DELAY = 3000;
+const EXIT_ANIMATION_DURATION = 800;
 
 export function Providers({ children }: { children: React.ReactNode }) {
+    const [mounted, setMounted] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [exiting, setExiting] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+
+        window.requestAnimationFrame(() => {
+            document.body.style.display = 'none';
+            document.body.offsetHeight;
+            document.body.style.display = '';
+        });
+
+        const timer = setTimeout(() => {
+            setExiting(true);
+            setTimeout(() => {
+                setLoading(false);
+            }, EXIT_ANIMATION_DURATION);
+        }, LOADING_DELAY);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    if (!mounted || loading) {
+        return (
+            <div className={`flex flex-col h-screen justify-center items-center text-center bg-gradient-to-tr from-black to-secondary
+                ${exiting ? 'animate-fade-out' : 'animate-fade-in'}`}>
+                <Image src={Logo} alt="Logo" width={320} height={320} className='py-8' />
+                <Spinner className='p-4' label="Rick's working his magic, just a moment..." labelColor='primary' color="primary" size="lg" />
+                <Image
+                    src="/images/global/rick.svg"
+                    alt="Rick svg"
+                    width={200}
+                    height={200}
+                    className='animate-delay-100 animate-appearance-in animate-iteration-infinite animate-duration-1000'
+                />
+            </div>
+        );
+    }
+
     return (
-        <NextUIProvider>
-            <NextThemesProvider attribute="class" defaultTheme="light">
-                {children}
-            </NextThemesProvider>
-        </NextUIProvider>
-    )
+        <ErrorBoundary>
+            <NextUIProvider>
+                <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem>
+                    {children}
+                </NextThemesProvider>
+            </NextUIProvider>
+        </ErrorBoundary>
+    );
 }
