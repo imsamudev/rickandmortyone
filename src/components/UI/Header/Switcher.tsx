f76c0090d34d85c309b37f73073da774726cebb9
SamuDev <samudev01@gmail.com>
2024-10-17 10:36:40 -0300

    Switcher updates

--- src/components/UI/Header/Switcher.tsx
+++ src/components/UI/Header/Switcher.tsx
@@ -1,18 +1,12 @@
 "use client";
-
 import { Switch } from "@nextui-org/react";
-import React, { ReactNode, useEffect, useState } from "react";
+import React, { ReactNode } from "react";
 import { SunIcon } from "./SunIcon";
 import { MoonIcon } from "./MoonIcon";
 import { useTheme } from "next-themes";
 
 const Switcher = () => {
-    const [mounted, setMounted] = useState(false);
     const { theme, setTheme } = useTheme();
-
-    useEffect(() => {
-        setMounted(true);
-    }, []);
 
     const handleSwitch = (isSelected: boolean, classname: string): ReactNode => {
         if (isSelected) {
@@ -24,11 +18,10 @@ const Switcher = () => {
         }
     };
 
-    if (!mounted) return null;
     return (
         <Switch
-            defaultSelected={theme === "light" ? true : false}
-            size="lg"
+            defaultSelected={theme === "light"}
+            size="md"
             color="primary"
             thumbIcon={({ isSelected, className }) => handleSwitch(isSelected, className)}
         />
