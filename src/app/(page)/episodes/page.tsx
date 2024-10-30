49864d478d851dfc4ee6b1b30e2eb8e24793ea39
SamuDev <samudev01@gmail.com>
2024-10-30 12:18:31 -0300

    Episodes styles changes

--- src/app/(page)/episodes/page.tsx
+++ src/app/(page)/episodes/page.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import TableEpisodes from '@/components/Episodes/TableEpisodes';
 import FilterEpisodes from '@/components/Episodes/FilterEpisodes';
-import { Spinner } from "@nextui-org/react";
+import { Divider, Spinner } from "@nextui-org/react";
 import { Episode } from '@/types/Episodes';
 import { PaginationInfo } from '@/types/Pagination';
 
@@ -61,9 +61,27 @@ const EpisodesPage: React.FC = () => {
 
     return (
         <div className="container mx-auto px-4 py-8">
-            <h1 className="text-3xl lg:text-5xl font-bold mb-6 text-center">Rick and Morty Episodes</h1>
+            <div className='h-[30vh]'>
+                <h1 className="text-5xl text-center md:text-right md:text-6xl lg:text-8xl drop-shadow-[-1.2px_1.2px_1.2px_rgba(0,0,0,0.8)] text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary dark:to-primary_1 mb-8 mt-6 py-4 animate-fade-in animate-duration-1000">
+                    <span className="text-2xl md:text-3xl lg:text-5xl text-transparent bg-clip-text bg-gradient-to-tr from-accent to-primary">
+                        Rick and Morty
+                    </span>
+                    <Divider orientation='horizontal' className='my-2' />
+                    Episodes
+                </h1>
+            </div>
 
-            <FilterEpisodes onSearch={handleSearch} loading={loading} />
+            <div className='text-center md:text-left'>
+                <p className='text-2xl md:text-3xl px-1 animate-fade-in animate-duration-1000'>
+                    Discover all the episodes available in the serie
+                </p>
+                <p className='text-base md:text-lg mb-4 px-1 animate-pulse animate-duration-1000 animate-infinite animate-delay-1000'>
+                    Start by searching for your favorite episodes!
+                </p>
+                <div className='w-[100%] flex justify-start mt-4'>
+                    <FilterEpisodes onSearch={handleSearch} loading={loading} />
+                </div>
+            </div>
 
             {loading ? (
                 <div className="w-full h-[100vh] flex justify-center items-center">
@@ -82,7 +100,7 @@ const EpisodesPage: React.FC = () => {
                             <button
                                 onClick={handlePrevPage}
                                 disabled={currentPage <= 1}
-                                className="px-4 py-2 bg-primary text-white rounded-lg disabled:bg-gray-300"
+                                className="px-4 py-2 bg-primary dark:bg-secondary_1 text-white dark:text-primary rounded-lg shadow-sm shadow-secondary dark:shadow-primary disabled:bg-primary_1 dark:disabled:bg-secondary hover:opacity-70 disabled:hover:opacity-100"
                             >
                                 Prev
                             </button>
@@ -90,7 +108,7 @@ const EpisodesPage: React.FC = () => {
                             <button
                                 onClick={handleNextPage}
                                 disabled={!paginationInfo || currentPage >= paginationInfo.pages}
-                                className="px-4 py-2 bg-primary text-white rounded-lg disabled:bg-gray-300"
+                                className="px-4 py-2 bg-primary dark:bg-secondary_1 text-white dark:text-primary rounded-lg shadow-sm shadow-secondary dark:shadow-primary disabled:bg-primary_1 dark:disabled:bg-secondary hover:opacity-70 disabled:hover:opacity-100"
                             >
                                 Next
                             </button>
