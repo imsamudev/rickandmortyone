51b9f7e0856a88e1ff6752f598e6c6dba300c7c9
SamuDev <samudev01@gmail.com>
2024-11-06 12:15:53 -0300

    Animation expand-contract

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,17 +11,18 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-      // The secondary colors "_number" are lighter
-          primary: '#42B4CA', //light blue 1
-          primary_1: '#86e3f6', //light blue 2
-          secondary: '#0A1128', //dark blue
-          secondary_1: '#121f49', //dark blue 2
-          accent: '#BFDE42', //limegreen
-          white: '#f3f3f3',
+        // The secondary colors "_number" are lighter
+        primary: '#42B4CA', //light blue 1
+        primary_1: '#86e3f6', //light blue 2
+        secondary: '#0A1128', //dark blue
+        secondary_1: '#121f49', //dark blue 2
+        accent: '#BFDE42', //limegreen
+        white: '#f3f3f3',
       },
       animation: {
         'fade-in': 'fadeIn .8s ease-in',
         'fade-out': 'fadeOut .8s ease-out',
+        'expand-contract': 'expandContract 3s infinite',
       },
       keyframes: {
         fadeIn: {
@@ -32,7 +33,11 @@ const config: Config = {
           '0%': { opacity: '1' },
           '100%': { opacity: '0' },
         },
-        }
+        expandContract: {
+          '0%, 100%': { filter: 'drop-shadow(0px 0px 2px rgba(191,222,66,1))' },
+          '50%': { filter: 'drop-shadow(0px 0px 6px rgba(191,222,66,1))' },
+        },
+      },
     },
   },
   darkMode: "class",
@@ -41,4 +46,5 @@ const config: Config = {
     require('tailwindcss-animated')
   ],
 };
+
 export default config;
