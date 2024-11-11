526e0e0c4498b4cf8d54d32672737a0bb8a1313d
SamuDev <samudev01@gmail.com>
2024-11-11 20:06:56 -0300

    ScrollIntoView trivia section

--- src/app/(page)/trivia/page.tsx
+++ src/app/(page)/trivia/page.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
-import Image from 'next/image'
+"use client"
+import React, { useRef, useEffect } from 'react';
+import Image from 'next/image';
 
 const TriviaGame = () => {
+    const triviaRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        if (triviaRef.current) {
+            triviaRef.current.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    }, []);
+
     return (
-        <div className='text-center h-[100vh] flex flex-col justify-center items-center animate-fade-in duration-500'>
-            <h1 className='text-4xl lg:text-7xl mb-4 drop-shadow-[-1.2px_1.2px_1.2px_rgba(0,0,0,1)]'>Coming Soon!</h1>
-            <h2 className='text-2xl lg:text-4xl animate-pulse duration-1000'>Get ready for a Rick and Morty trivia challenge!</h2>
+        <div
+            ref={triviaRef}
+            className='text-center h-[100vh] flex flex-col justify-center items-center animate-fade-in duration-500'
+        >
+            <h1 className='text-4xl lg:text-7xl mb-4 drop-shadow-[-1.2px_1.2px_1.2px_rgba(0,0,0,1)]'>
+                Coming Soon!
+            </h1>
+            <h2 className='text-2xl lg:text-4xl animate-pulse duration-1000'>
+                Get ready for a Rick and Morty trivia challenge!
+            </h2>
             <Image
                 src='/images/global/rym3.png'
                 alt='Rick and Morty image'
@@ -14,9 +33,8 @@ const TriviaGame = () => {
                 objectFit='cover'
                 className='block my-8 sm:w-72 lg:w-96 animate-expand-contract'
             />
-
         </div>
-    )
-}
+    );
+};
 
-export default TriviaGame
+export default TriviaGame;
